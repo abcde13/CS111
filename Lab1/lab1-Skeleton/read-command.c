c0f04401f91cf66c42b385dcc68ca4e922e34d62
Arjun Lakshmipathy <lakshmipathyarjun6@gmail.com>
2014-01-11 14:16:24 -0800

    Struct decleared

--- Lab1/lab1-Skeleton/read-command.c
+++ Lab1/lab1-Skeleton/read-command.c
@@ -7,9 +7,13 @@
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
-
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
+
+struct command_stream
+{
+    char **forest_pointer;
+};
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
