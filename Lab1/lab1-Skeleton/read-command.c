ff86048cd121f9d4114d40344b385d141ea50e15
Joraaver Chahal <machspeeds@gmail.com>
2014-01-11 15:35:44 -0800

    BROKEN! fixing & and |

--- Lab1/lab1-Skeleton/read-command.c
+++ Lab1/lab1-Skeleton/read-command.c
@@ -24,35 +24,84 @@ make_command_stream (int (*get_next_byte) (void *),
      You can also use external functions defined in the GNU C Library.  */
   //error (1, 0, "command reading not yet implemented");	
     int c = get_next_byte(get_next_byte_argument);
-    int Andflag = 0;
+    int andFlag = 0;
+    int orFlag = 0;
     
     while(c!=EOF){
         switch (c) {
             case '<' :
                 printf("< \n");
+		if(andFlag){
+			printf("Error \n");
+			andFlag = 0;
+		} else if(orFlag){
+			printf("Pipe \n");
+			orFlag = 0;
+		}
                 break;
             case '>' :
-                printf("> \n");
+		printf("> \n");
+                if(andFlag){
+			printf("Error \n");
+			andFlag = 0;
+		} else if(orFlag){
+			printf("Pipe \n");
+			orFlag = 0;
+		}
                 break;
             case '&' :
-                printf("& \n");
-                c = get_next_byte(get_next_byte_argument);
-                if(c!='&')
-                    printf("failed");
-                break;
+		printf("%c \n", c);
+                if(andFlag){
+			andFlag = 0;
+			printf("And \n");
+		} else {
+			andFlag = 1;
+		}
+		if(orFlag){
+			printf("Pipe \n");
+			orFlag = 0;
+		}
             case '|' :
-                printf("| \n");
-                c = get_next_byte(get_next_byte_argument);
-                if(c!='|')
-                    printf("it's a pipeline");
+                printf("%c \n", c);
+                if(andFlag){
+			printf("Error \n");
+			andFlag = 0;
+		} else {
+			if(orFlag){
+				orFlag = 0;
+				printf("Or \n");
+			} else {
+				orFlag = 1;
+			}
+		}
                 break;
             case '(' :
                 printf("( \n");
+                if(andFlag){
+			printf("Error \n");
+			andFlag = 0;
+		} else if(orFlag){
+			printf("Pipe \n");
+			orFlag = 0;
+		}
                 break;
             case ')' :
                 printf(") \n");
+                if(andFlag){
+			printf("Error \n");
+			andFlag = 0;
+		} else if(orFlag) {
+			printf("Pipe \n");
+			orFlag = 0;
+		}
                 break;
             default :
+                if(andFlag){
+			printf("Error \n");
+		} else if(orFlag){
+			printf("Pipe \n");
+			orFlag = 0;
+		}
                 break;
         }
         c = get_next_byte(get_next_byte_argument);
