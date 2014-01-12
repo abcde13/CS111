ddee73e9f9a38385b5f1aba1d5d25c5df2ba22dc
Joraaver Chahal <machspeeds@gmail.com>
2014-01-11 18:23:49 -0800

    preparing for stack

--- Lab1/lab1-Skeleton/read-command.c
+++ Lab1/lab1-Skeleton/read-command.c
@@ -5,16 +5,29 @@
 #include <stdio.h>
 #include <error.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
 
+const int AND_OPERATOR = 0;
+const int OR_OPERATOR = 1;
+const int PIPE_OPERATOR = 2;
+const int REDIRECT_FROM = 3;
+const int REDIRECT_TO = 4;
+const int OPEN_PAREN = 5;
+const int CLOSE_PAREN = 6;
+const int NEWLINE  = 7;
+const int SPACE = 8;
+
 struct command_stream
 {
     command_t **forest_pointer;
 };
+
+void add_to_stack(int constant, char * word, int length);
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
@@ -29,15 +42,9 @@ make_command_stream (int (*get_next_byte) (void *),
     int orFlag = 0;
     int wordFlag = 0;
     int spaceFlag = 0;
-    const int AND_OPERATOR = 0;
-    const int OR_OPERATOR = 1;
-    const int PIPE_OPERATOR = 2;
-    const int REDIRECT_FROM = 3;
-    const int REDIRECT_TO = 4;
-    const int OPEN_PAREN = 5;
-    const int CLOSE_PAREN = 6;
-    const int NEWLINE  = 7;
-    const int SPACE = 8;
+    int size = 100;
+    int buffcount = 0;
+    char * buff = malloc(sizeof(char) * size);
     
     while(c!=EOF){
 	if(c == ' ' && spaceFlag){
@@ -51,48 +58,94 @@ make_command_stream (int (*get_next_byte) (void *),
 	}
 	if(spaceFlag && wordFlag){
 		wordFlag = 0;
+		int i = 0;
+		while(i != buffcount){
+		  printf("%c", buff[i]);
+		  i++;
+		}
+		printf("\n");
+		buffcount = 0;
+		printf("WORD \n");
+		add_to_stack(SPACE,NULL,-1);
+		
 	}
 	spaceFlag = 0;
         switch (c) {
             case '<' :
-                printf("< \n");
-		wordFlag = 0;
+		if(wordFlag){
+			wordFlag = 0;
+			int i = 0;
+			while(i != buffcount){
+			  printf("%c", buff[i]);
+			  i++;
+			}
+			buffcount = 0;
+			printf("WORD \n");
+		}
 		if(andFlag){
 			printf("Error \n");
 			andFlag = 0;
 		} else if(orFlag){
-			printf("Pipe \n");
 			orFlag = 0;
+			add_to_stack(PIPE_OPERATOR,NULL,-1);
 		}
+		add_to_stack(REDIRECT_FROM,NULL,-1);
                 break;
             case '>' :
-		printf("> \n");
-		wordFlag = 0;
+		if(wordFlag){
+			wordFlag = 0;
+			int i = 0;
+			while(i != buffcount){
+			  printf("%c", buff[i]);
+			  i++;
+			}
+			buffcount = 0;
+			printf("WORD \n");
+		}
                 if(andFlag){
 			printf("Error \n");
 			andFlag = 0;
 		} else if(orFlag){
-			printf("Pipe \n");
+			add_to_stack(PIPE_OPERATOR,NULL,-1);
 			orFlag = 0;
 		}
+		add_to_stack(REDIRECT_FROM,NULL,-1);
                 break;
             case '&' :
 		printf("%c \n", c);
-		wordFlag = 0;
+		if(wordFlag){
+			wordFlag = 0;
+			int i = 0;
+			while(i != buffcount){
+			  printf("%c", buff[i]);
+			  i++;
+			}
+			buffcount = 0;
+			printf("WORD \n");
+		}
                 if(andFlag){
 			andFlag = 0;
-			printf("And \n");
+			add_to_stack(AND_OPERATOR,NULL,-1);
 		} else {
 			andFlag = 1;
-		}
-		if(orFlag){
-			printf("Pipe \n");
+		} 
+		if(orFlag){ 
 			orFlag = 0;
+			add_to_stack(PIPE_OPERATOR,NULL,-1);
 		}
 		break;
             case '|' :
                 printf("%c \n", c);
-		wordFlag = 0;
+		if(wordFlag){
+			wordFlag = 0;
+			int i = 0;
+			while(i != buffcount){
+			  printf("%c", buff[i]);
+			  i++;
+			}
+			buffcount = 0;
+			printf("WORD \n");
+		}
                 if(andFlag){
 			printf("Error \n");
 			andFlag = 0;
@@ -100,37 +153,72 @@ make_command_stream (int (*get_next_byte) (void *),
 		} else {
 			if(orFlag){
 				orFlag = 0;
-				printf("Or \n");
+				add_to_stack(OR_OPERATOR,NULL,-1);
 			} else {
 				orFlag = 1;
 			}
 		}
                 break;
             case '(' :
-                printf("( \n");
-		wordFlag = 0;
+		if(wordFlag){
+			wordFlag = 0;
+			int i = 0;
+			while(i != buffcount){
+			  printf("%c", buff[i]);
+			  i++;
+			}
+			buffcount = 0;
+			printf("WORD \n");
+		}
                 if(andFlag){
 			printf("Error \n");
 			andFlag = 0;
 		} else if(orFlag){
-			printf("Pipe \n");
+			add_to_stack(PIPE_OPERATOR,NULL,-1);
 			orFlag = 0;
 		}
+		add_to_stack(OPEN_PAREN,NULL,-1);
                 break;
             case ')' :
-                printf(") \n");
-		wordFlag = 0;
+		if(wordFlag){
+			wordFlag = 0;
+			int i = 0;
+			while(i != buffcount){
+			  printf("%c", buff[i]);
+			  i++;
+			}
+			buffcount = 0;
+			printf("WORD \n");
+		}
                 if(andFlag){
 			printf("Error \n");
 			andFlag = 0;
 		} else if(orFlag) {
-			printf("Pipe \n");
+			add_to_stack(PIPE_OPERATOR,NULL,-1);
 			orFlag = 0;
 		}
+		add_to_stack(CLOSE_PAREN,NULL,-1);
                 break;
 	    case '\n' : 
 	    case ';':
-		wordFlag = 0;
+		if(orFlag){
+			add_to_stack(PIPE_OPERATOR,NULL,-1);	
+			orFlag = 0;
+		} else if (andFlag){
+			printf("Error");
+			andFlag = 0;
+		}  
+		if(wordFlag){
+			wordFlag = 0;
+			int i = 0;
+			while(i != buffcount){
+			  printf("%c", buff[i]);
+			  i++;
+			}
+			buffcount = 0;
+			printf("WORD \n");
+		}
+		add_to_stack(NEWLINE,NULL,-1);
 		break;
             default :
 		if(!isalnum(c) && c!='!'&& c!='%' && c!='+'&& c!=',' && c!='-' && c!='.' &&  c!='/'&&  c!=':'&& c!='@'&& c!='^'&& c!='_'){
@@ -140,10 +228,16 @@ make_command_stream (int (*get_next_byte) (void *),
 				printf("Error \n");
 				andFlag =0;
 			} else if(orFlag){
-				printf("Pipe \n");
+				add_to_stack(PIPE_OPERATOR,NULL,-1);
 				orFlag = 0;
 			} else {
 				printf("%c \n", c);
+				if(buffcount == size){
+					size = size + 100;
+					buff = realloc(buff,sizeof(size));
+				}
+			        buff[buffcount] = c;
+				buffcount++;
 				wordFlag = 1;
 			}			
 		}
@@ -152,6 +246,11 @@ make_command_stream (int (*get_next_byte) (void *),
         }
         c = get_next_byte(get_next_byte_argument);
     }
+	if(orFlag){
+		add_to_stack(PIPE_OPERATOR,NULL,-1);	
+	} else if (andFlag){
+		printf("Error");
+	}  
   return 0;
 }
 
@@ -164,3 +263,29 @@ read_command_stream (command_stream_t s)
 }
 
 
+void add_to_stack(int constant, char * word, int length){
+	if(word != NULL){
+	}
+	else {
+		if(constant == AND_OPERATOR){
+			printf("I have an AND \n");
+		} else if(constant == OR_OPERATOR){
+			printf("I have an OR \n");
+		} else if(constant == PIPE_OPERATOR){
+			printf("I have an PIPE \n");
+		}  else if(constant == REDIRECT_FROM){
+			printf("I have an < \n");
+		}  else if(constant == REDIRECT_TO){
+			printf("I have an > \n");
+		}  else if(constant == NEWLINE){
+			printf("I have an newline/; \n");
+		}  else if(constant == SPACE){
+			printf("I have an space \n");
+		}  else if(constant == CLOSE_PAREN){
+			printf("I have an ) \n");
+		}  else if(constant == OPEN_PAREN){
+			printf("I have an ( \n");
+		}
+	}
+}
+		
