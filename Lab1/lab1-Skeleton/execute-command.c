e3e34280698b106c5e8d9ac386920bd10e7bc35e
Arjun <lakshmipathy6@lakshmipathy6-laptop.(none)>
2014-01-21 00:02:30 -0800

    Lab 1b begins

--- Lab1/lab1-Skeleton/execute-command.c
+++ Lab1/lab1-Skeleton/execute-command.c
@@ -18,6 +18,12 @@
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
+void do_command (command_t c);
+void execute_and_operator (command_t c);
+void execute_or_operator (command_t c);
+void execute_pipe_operator (command_t c);
+void execute_simple_command (command_t c);
+
 int
 command_status (command_t c)
 {
@@ -27,8 +33,83 @@ command_status (command_t c)
 void
 execute_command (command_t c, int time_travel)
 {
-  /* FIXME: Replace this with your implementation.  You may need to
-     add auxiliary functions and otherwise modify the source code.
-     You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command execution not yet implemented");
+  /*	FIXME: Replace this with your implementation.  You may need to
+     	add auxiliary functions and otherwise modify the source code.
+    	You can also use external functions defined in the GNU C Library.  */
+	do_command(c);
+	//	error (1, 0, "command execution not yet implemented");
+}
+
+void
+do_command (command_t c)
+{
+	switch(c->type)
+	{
+		case AND_COMMAND:
+			execute_and_operator(c);
+			break;
+		case OR_COMMAND:
+			execute_or_operator(c);
+			break;
+		case SIMPLE_COMMAND:
+			execute_simple_command(c);
+			break;
+		default:
+			error(1,0,"Command not found. Try again fucker.");
+			break;
+	} 
+}
+
+void 
+execute_and_operator (command_t c)
+{
+	do_command(c->u.command[0]);
+	printf("%d",c->u.command[0]->status);
+
+	if(c->u.command[0]->status == 0)
+	{
+		do_command(c->u.command[1]);
+		c->status = c->u.command[1]->status;
+	}
+	else
+	{
+		c->status = c->u.command[0]->status;
+	}	
+}
+
+void 
+execute_or_operator (command_t c)
+{
+	do_command(c->u.command[0]);
+
+	if(!(c->u.command[0]->status))
+	{
+		c->status = c->u.command[0]->status;
+	}
+	else
+	{
+		do_command(c->u.command[1]);
+		c->status = c->u.command[1]->status;
+	}	
+}
+
+void
+execute_pipe_operator (command_t c)
+{
+}
+
+void
+execute_simple_command (command_t c)
+{
+//	pid_t pid = fork();
+	
+//	if(!pid)
+//	{
+		execvp(c->u.word[0],c->u.word);
+		exit(c->status);
+//	}
+//	else
+//	{
+//		error(1,0,"You forked up");
+//	}
 }
